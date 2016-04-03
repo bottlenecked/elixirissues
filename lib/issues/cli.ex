1431548bb6ccbe435ee0186dfb5e36a89106bc29
Manos Em <esemmano@gmail.com>
2016-04-03 19:21:46 +0300

    my take on the final step of the test project

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -9,9 +9,6 @@ defmodule Issues.CLI do
         argv
         |> parse_args
         |> process
-        |> decode_response
-        #|> convert_to_list_of_maps
-        |> sort_into_ascending_order
     end
     
     @doc """
@@ -41,9 +38,24 @@ defmodule Issues.CLI do
         System.halt(0)
     end
     
-    def process ({user, project, _count}) do
-        Issues.GithubIssues.fetch(user, project)
+    def process ({user, project, count}) do
+        items = Issues.GithubIssues.fetch(user, project)
         |> decode_response
+        |> convert_to_list_of_maps
+        |> sort_into_ascending_order
+        |> Enum.take(count)
+        |> get_req_fields
+        |> append_headers
+        
+        len = count_lengthiest items
+        
+        [ljhead|ljtail] = for item<-items do
+            {String.ljust(elem(item, 0), elem(len,0)), String.ljust(elem(item, 1), elem(len,1)), String.ljust(elem(item, 2), elem(len,2))}
+        end
+        
+        final = [ljhead | [{ String.rjust("", elem(len,0), ?-), String.rjust("", elem(len,1), ?-), String.rjust("-", elem(len,2), ?-)} | ljtail]]
+        
+        for item<-final, do: print elem(item, 0), elem(item, 1), elem(item, 2)
     end
     
     def decode_response({:ok, body}) do
@@ -64,4 +76,27 @@ defmodule Issues.CLI do
     def sort_into_ascending_order(list) do
         Enum.sort list, fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
     end
+    
+    def get_req_fields(list) do
+        for map<-list do
+            {Integer.to_string(map["number"])<>" ", " "<>map["created_at"]<>" ", " "<>map["title"]}
+        end
+    end
+    
+    def append_headers(list) do
+        [{" # ", " created_at ", " title "} | list]
+    end
+    
+    def count_lengthiest(list, a\\0, b\\0, c\\0)
+    
+    def count_lengthiest([], a, b, c) do
+        {a,b,c}
+    end
+    
+    def count_lengthiest([head|tail], a, b ,c) do
+        count_lengthiest tail, max(a, String.length elem(head, 0)), max(b, String.length elem(head, 1)), max(c, String.length elem(head, 2))
+    end
+    
+    def print(number="-"<>_, date, title), do: IO.puts "#{number}+#{date}+#{title}"
+    def print(number, date, title), do: IO.puts "#{number}|#{date}|#{title}"
 end
